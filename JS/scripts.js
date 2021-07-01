8ff75f3e3346db3ba5e48c709c09f575fe658ec9
Andrey Skoroplyas <andrey-skoroplyas@yandex.ru>
2021-07-01 14:58:01 +0300

    Work 01.07.2021

--- JS/scripts.js
+++ JS/scripts.js
@@ -40,7 +40,78 @@ $(function(){
     });
     
     $(document).on('click', 'button[type="submit"]', function(){
-        alert('OGOGO!!!')
+        alert('OGOGO!!!');
     });
     
+    $(document).on('click', '.order .del > div', function(){
+        tovarDelete(this);
+    });
+    
+    $(document).on('input', '.order .num > input', function(){
+        tovarChange(this);
+    });
 });
+
+
+
+/* order */
+const order = [
+    {
+        id: 5711,
+        value: 10
+    },
+    {
+        id: 3432,
+        value: 10
+    },
+    {
+        id: 4846,
+        value: 10
+    }
+]
+function tovarDelete(point) {
+    let b = point.parentNode.parentNode;
+    let t_id = b.querySelector('th').dataset.tovar;
+    for (let i = 0; i < order.length; i++) {
+        if (order[i].id == t_id) {
+            order.splice(i, 1);
+            console.log(t_id); // имитация отправки бэку сообщения об удалении товара
+            break;
+        }
+    }
+    b.remove();
+    if (order.length > 0) {
+        tovarCount();
+    } else {
+        orderEmpty();
+    }
+}
+function tovarChange(point) {
+    let new_quantity = point.value;
+    if (new_quantity <= 0) {
+        tovarDelete(point);
+    } else {
+        let tovar_id = point.parentNode.parentNode.querySelector('th').dataset.tovar;
+        for (let i = 0; i < order.length; i++) {
+            if (order[i].id == tovar_id) {
+                order[i].value = new_quantity;
+                console.log(order[i]);
+                break;
+            }
+        }
+        tovarCount();
+    }
+}
+function tovarCount() { // пересчет товара
+    let itog = 0; // общий итог
+    for (let i = 0; i < order.length; i++) { // перебираем в цикле корзину
+        const row = $('.order .table tbody tr').eq(i); // берем строку, соответствующую по порядковому номеру перебираемому товару в корзине
+        row.find('th').html(i + 1); // выставляем порядковый номер товара. +1 потому что надо считать с 1, а у нас счет с 0
+        row.find('.sum').html(row.find('.rub').html() * order[i].value); // в ячейку с классом sum кладем произведение количества товара, взятого из корзины, на цену товара, взятую из ячейки с классом rub
+        itog += +row.find('.sum').html(); // плюсуем к итогу содержимое ячейки с классом sum (произведение количества на цену нашего товара)
+    }
+    $('.order .table .allsum').html(itog); // кладем итог в ячейку с классом allsum
+}
+function orderEmpty() {
+    $('.order').addClass('empty');
+}
