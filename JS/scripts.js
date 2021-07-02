eca3a5d8f4af0ba086a187062e6c8e1415707fe9
Andrey Skoroplyas <andrey-skoroplyas@yandex.ru>
2021-07-02 14:57:29 +0300

    Work 02.07.2021

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,7 +9,7 @@ $(function(){
                 height: 0
             }, 1000);
             $(this).parent().addClass('open').find('.submenu').animate({
-                height: ($(this).parent().find('.submenu a').length * 24)
+                height: ($(this).parent().find('.submenu a').length * $('.submenu a').height())
             }, 1000);
         }
     });
@@ -39,10 +39,6 @@ $(function(){
         }
     });
     
-    $(document).on('click', 'button[type="submit"]', function(){
-        alert('OGOGO!!!');
-    });
-    
     $(document).on('click', '.order .del > div', function(){
         tovarDelete(this);
     });
@@ -50,68 +46,20 @@ $(function(){
     $(document).on('input', '.order .num > input', function(){
         tovarChange(this);
     });
+    
+    $('#date').click(function(){
+        if ($('#date').val()) {
+            selected_day = makeSelectedDate($('#date').val());
+            makePopup(selected_day.getFullYear(),selected_day.getMonth());
+        } else {
+            makePopup(TODAY.getFullYear(),TODAY.getMonth());
+        }
+    });
+    
+    $('#date').mask('00-00-0000');
+    
+    $('#orderdata').on('submit', function(e){// отправка формы
+        e.preventDefault();
+        orderAction();
+    })
 });
-
-
-
-/* order */
-const order = [
-    {
-        id: 5711,
-        value: 10
-    },
-    {
-        id: 3432,
-        value: 10
-    },
-    {
-        id: 4846,
-        value: 10
-    }
-]
-function tovarDelete(point) {
-    let b = point.parentNode.parentNode;
-    let t_id = b.querySelector('th').dataset.tovar;
-    for (let i = 0; i < order.length; i++) {
-        if (order[i].id == t_id) {
-            order.splice(i, 1);
-            console.log(t_id); // имитация отправки бэку сообщения об удалении товара
-            break;
-        }
-    }
-    b.remove();
-    if (order.length > 0) {
-        tovarCount();
-    } else {
-        orderEmpty();
-    }
-}
-function tovarChange(point) {
-    let new_quantity = point.value;
-    if (new_quantity <= 0) {
-        tovarDelete(point);
-    } else {
-        let tovar_id = point.parentNode.parentNode.querySelector('th').dataset.tovar;
-        for (let i = 0; i < order.length; i++) {
-            if (order[i].id == tovar_id) {
-                order[i].value = new_quantity;
-                console.log(order[i]);
-                break;
-            }
-        }
-        tovarCount();
-    }
-}
-function tovarCount() { // пересчет товара
-    let itog = 0; // общий итог
-    for (let i = 0; i < order.length; i++) { // перебираем в цикле корзину
-        const row = $('.order .table tbody tr').eq(i); // берем строку, соответствующую по порядковому номеру перебираемому товару в корзине
-        row.find('th').html(i + 1); // выставляем порядковый номер товара. +1 потому что надо считать с 1, а у нас счет с 0
-        row.find('.sum').html(row.find('.rub').html() * order[i].value); // в ячейку с классом sum кладем произведение количества товара, взятого из корзины, на цену товара, взятую из ячейки с классом rub
-        itog += +row.find('.sum').html(); // плюсуем к итогу содержимое ячейки с классом sum (произведение количества на цену нашего товара)
-    }
-    $('.order .table .allsum').html(itog); // кладем итог в ячейку с классом allsum
-}
-function orderEmpty() {
-    $('.order').addClass('empty');
-}
